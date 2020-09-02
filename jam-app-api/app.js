39526c029cebf19e80f99972276590483899c2bf
Tom Jane <kodemonki@hotmail.com>
2020-09-02 11:43:38 +0100

    added fe

--- jam-app-api/app.js
+++ jam-app-api/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("express-jwt");
+
 const app = express();
 const secret = "secret";
 
-app.post("/auth", (req, res) => {
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.get("/auth", (req, res) => {
+  console.log(req.body)
   const token = jsonwebtoken.sign({ foo: "bar" }, secret);
   res.send(token);
 });
