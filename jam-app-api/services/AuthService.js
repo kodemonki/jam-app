205192dd2fad5067919cea8c7d32e5950a9ba3dd
Tom Jane <kodemonki@hotmail.com>
2020-09-03 21:56:31 +0100

    Update AuthService.js

--- jam-app-api/services/AuthService.js
+++ jam-app-api/services/AuthService.js
@@ -2,15 +2,15 @@ const jsonwebtoken = require("jsonwebtoken");
 const config = require("config");
 
 class AuthService {
-
   getToken(username, password) {
-
     /**
      * assume its valid
      */
 
     const payload = { auth: "true" };
-    const token = jsonwebtoken.sign(payload, config.get("jwt.secret"));
+    const token = jsonwebtoken.sign(payload, config.get("jwt.secret"), {
+      expiresIn: 24 * 60 * 60,
+    });
 
     return { jwt: token };
   }
