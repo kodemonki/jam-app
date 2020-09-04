d4532da3e23afd9a6d20a703835cdaf45d0994a8
Tom Jane <kodemonki@hotmail.com>
2020-09-04 23:43:11 +0100

    Update Login.js

--- jam-app-fe/src/components/Login.js
+++ jam-app-fe/src/components/Login.js
@@ -2,13 +2,13 @@ import React from "react";
 import axios from "axios";
 
 function Login(props) {
-
   const getToken = (e) => {
     axios
       .get("http://localhost:3001/login?u=Tom&p=password")
       .then((response) => {
-        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
-        props.setUserData(response.data)
+        axios.defaults.headers.common["Authorization"] =
+          "Bearer " + response.data.jwt;
+        props.setUserData(response.data);
         props.history.push(props.nextRoute);
       })
       .catch((error) => {
@@ -18,7 +18,16 @@ function Login(props) {
 
   return (
     <div className="Login">
-      <p>Login<br/>------</p>
+      <p>
+        Login
+        <br />
+        ------
+      </p>
+      <form>
+        <label>username</label><br/><input name="username" type="text" value="Tom"/><br/>
+        <label>password</label><br/><input name="password" type="password" value="password"/>
+      </form>
+      <p>------</p>
       <button onClick={getToken}>Submit</button>
     </div>
   );
